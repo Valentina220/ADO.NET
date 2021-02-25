eb81c67b52afa254e6ad245367ea98c394f1c7d9
Valentina220 <73115404+Valentina220@users.noreply.github.com>
2021-02-25 19:33:15 +0300

    Add files via upload

---
+++ ADO.NET_EntityFramework/Practice_9/Exercise2_3/CodeFirst/SampleContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFirst
+{
+    public class SampleContext : DbContext
+    {
+        public SampleContext() : base("MyShop")
+        {
+            //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<SampleContext>());
+        }
+        public DbSet<Model.Customer> Customers { get; set; }
+        public DbSet<Model.Order> Orders { get; set; }
+        public DbSet<Model.VipOrder> VipOrders { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Model.Customer>()
+            .Property(c => c.LastName).IsRequired().HasMaxLength(30);
+        }
+
+    }
+}
